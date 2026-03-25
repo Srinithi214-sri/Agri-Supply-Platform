2208731514215a675b4e076453855ee01a5f4b9a
Srivatsan V <srivatsan116@gmail.com>
2026-03-26 04:42:55 +0530

    Login-page added successfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
-import MandiPrices from "./MandiPrices";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import FarmerLogin from './FarmerLogin';
+import FarmerSignup from './FarmerSignup';
+import MandiPrices from './MandiPrices';
 
 function App() {
-
- return (
-
-  <div>
-
-   <MandiPrices/>
-
-  </div>
-
- );
-
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FarmerLogin />} />
+        <Route path="/signup" element={<FarmerSignup />} />
+        <Route path="/dashboard" element={<MandiPrices />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
