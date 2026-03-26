cf78fb754d977e03c56a2e7e694fe73a6a8ff4ac
Srinithi <premak602@gmail.com>
2026-03-26 06:11:52 -0700

    shipment successfull

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mandiRoutes from "./routes/mandiRoutes.js";
+import shipmentRoutes from "./routes/shipmentRoutes.js";
+import connectdb from "./config/db.js";
 
 dotenv.config();
+connectdb();
 
 const app = express();
 
@@ -11,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/mandi", mandiRoutes);
+app.use("/api/shipments", shipmentRoutes);
 
 app.listen(5000, () => {
  console.log("Server running on port 5000");
